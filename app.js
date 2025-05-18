e5063acbccecd35844911249afe82261a7866f96
Mariana <mariianaf3@gmail.com>
2025-05-18 16:54:31 -0400

    Configurando path uploads

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const rolRoutes = require('./routes/rolRoutes');
 const webpayRoutes = require('./routes/webpayRoutes');
 
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
@@ -21,6 +22,8 @@ app.use('/ferremas/sucursales', comunaRoutes);
 app.use('/ferremas/empleados', empleadoRoutes);
 app.use('/ferremas/roles', rolRoutes);
 app.use('/ferremas/webpay', webpayRoutes);
+// Agregar esta línea a tu app.js
+app.use('/ferremas/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const PORT = 3000;
 app.listen(PORT, () => {
